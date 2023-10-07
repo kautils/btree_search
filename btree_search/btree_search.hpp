a7d1b35241484e179adf9fd47151a0723d2c582f
ka <todo1@rotarydef.net>
2023-10-07 14:54:05 -0700

    api

--- btree_search/btree_search.hpp
+++ btree_search/btree_search.hpp
@@ -14,117 +14,24 @@ struct btree_search{
     btree_search(pref * prf) : prf(prf){}
     
     /// @return true if find [want]
-    
-    /** @note side-effect  
-     *  [nearest_pos]
-     *  [nearest_direction] : (-1,0,1) (left,exact,right) 
-     **/
-    bool search(value_type const& want,offset_type * nearest_pos = nullptr, int * nearest_direction = nullptr){
-        
-        struct value_info{
-            value_type ent;
-            value_type *v=&ent; // in case of wanting to avoid copying value. for future, especially bigint. 
-            offset_type b = offset_type(0);
-            bool done=false;
-            bool break_bit = false;
-        };
-        
-        auto l = value_info{};
-        auto r = value_info{};
-        
-        bool exact = false;
-        auto res = short(-2);
-        auto pos = offset_type(0);
-        auto lower_limit_size = offset_type(0);
-        auto upper_limit_size = prf->max_size();
-        auto max_size = upper_limit_size; 
-        auto min_size = offset_type(0);
-        auto entire_direction = 1;
-        auto entire_direction_init = false;
-        auto block_size = prf->block_size();
-        //auto is_continue = true;
-        auto max_size__ = offset_type (0); 
-        auto min_size__ = offset_type (0); 
-        
-        
-        for(;res;){
-            auto mid = ((max_size-min_size) /  2 + min_size);
-            auto adj = mid % (block_size);
-            
-            l.b =     (mid > adj + (block_size))*static_cast<offset_type>(mid - adj - (block_size))
-                    +!(mid > adj + (block_size))*lower_limit_size;
-            r.b = (mid > adj)*static_cast<offset_type>(mid - adj ) + !(mid > adj)*lower_limit_size;
-            
-            // confirm to check both pole(lb,rb) because i ignore calclation of rb if possible.
-            if( (2==(max_size==max_size__)+ (min_size==min_size__)) + r.break_bit +  (l.b >=upper_limit_size) + (r.b>=upper_limit_size) + (l.b<lower_limit_size) + (r.b<lower_limit_size))break;
-            min_size__=min_size; 
-            max_size__=max_size;
-            
-            prf->read_value(l.b,&l.v);
-            l.done=true;
-            res = /*((want == lv) * 0) +*/ ((want < *l.v)*-1) + (!(want < *l.v)/* *1 */);  
-            pos=static_cast<offset_type>(l.b-(block_size)*bool(res));
-
-            if(res==1){
-                prf->read_value(r.b,&r.v);
-                r.done=true;
-                res = /*((want == rv) * 0) +*/ ((want > *r.v)/* *1 */);  
-                r.break_bit = !(want > *r.v);
-                pos=static_cast<offset_type>(r.b+(block_size*bool(res)));
-            }
-
-            
-            entire_direction = (entire_direction_init)*entire_direction + !(entire_direction_init)*res; 
-            entire_direction_init=true;
-
-            pos= (res>0)*pos;
-            max_size=
-                  (res>0)*((entire_direction==-1)*pos +!(entire_direction==-1)*max_size)
-                +!(res>0)*offset_type(l.b); 
-            
-            min_size=static_cast<offset_type>(
-                  (res>0)*((entire_direction==-1)*(r.b+(block_size)) +!(entire_direction==-1)*pos)
-                +!(res>0)*min_size );
-            //printf("%d %ld [%lld %lld] (%lld %lld) |%ld %ld|\n",res,pos,l.b,r.b,*l.v,*r.v,min_size,max_size); fflush(stdout);
-        }
-        exact = r.done*(want == *r.v) + r.done*(want == *l.v);
-        res = r.break_bit + !r.break_bit*res ;
-        
-        //printf("---- %d %ld [%lld %lld] (%lld %lld) |%ld %ld|\n",res,pos,l.b,r.b,*l.v,*r.v,min_size,max_size); fflush(stdout);
-        
-        if(nearest_pos){
-            auto a = (want>*r.v)*(want - *r.v)+!(want>*r.v)*(*r.v-want); 
-            auto b = (want>*l.v)*(want - *l.v)+!(want>*l.v)*(*l.v-want); 
-            *nearest_pos = static_cast<offset_type>(!(a<b))*l.b + static_cast<offset_type>(a<b)*r.b; // if abs is equal, l is prior 
-            if(nearest_direction)*nearest_direction= !exact*res;
-        }
-        
-        return exact;
-    }
-    
-    
-    
     struct btree_search_result{
         //offset_type * nearest_pos;
+        value_type value;
+        offset_type pos; 
         
-        value_type lv;
-        offset_type lpos; 
+        value_type next_value;
+        offset_type next_pos; 
         
-        value_type rv;
-        offset_type rpos; 
-        
-        bool found = false;
-        
-        
+        int direction = 0;
+        bool overflow = false;
     };
 
 
-    
     /** @note side-effect  
      *  [nearest_pos]
      *  [nearest_direction] : (-1,0,1) (left,exact,right) 
      **/
-    btree_search_result __search(value_type const& want){
+    btree_search_result search(value_type const& want,int detail=true /* true/false */){
         
         struct value_info{
             value_type ent;
@@ -150,7 +57,8 @@ struct btree_search{
         //auto is_continue = true;
         auto max_size__ = offset_type (0); 
         auto min_size__ = offset_type (0); 
-        
+        auto is_overflow_max=false;
+        auto is_overflow_min=false;
         
         for(;res;){
             auto mid = ((max_size-min_size) /  2 + min_size);
@@ -160,8 +68,18 @@ struct btree_search{
                     +!(mid > adj + (block_size))*lower_limit_size;
             r.b = (mid > adj)*static_cast<offset_type>(mid - adj ) + !(mid > adj)*lower_limit_size;
             
+            is_overflow_max=(l.b >=upper_limit_size)+(r.b>=upper_limit_size);
+            is_overflow_min=(l.b<lower_limit_size) +(r.b<lower_limit_size);
             // confirm to check both pole(lb,rb) because i ignore calclation of rb if possible.
-            if( (2==(max_size==max_size__)+ (min_size==min_size__)) + r.break_bit +  (l.b >=upper_limit_size) + (r.b>=upper_limit_size) + (l.b<lower_limit_size) + (r.b<lower_limit_size))break;
+            if((2==(max_size==max_size__)+ (min_size==min_size__)) 
+                +r.break_bit
+                +is_overflow_max
+                +is_overflow_min){
+                break;
+            } 
+//                +(l.b >=upper_limit_size)+(r.b>=upper_limit_size) 
+//                +(l.b<lower_limit_size) +(r.b<lower_limit_size)) break;
+            
             min_size__=min_size; 
             max_size__=max_size;
             
@@ -194,69 +112,28 @@ struct btree_search{
         }
         exact = r.done*(want == *r.v) + r.done*(want == *l.v);
         res = r.break_bit + !r.break_bit*res ;
+//        printf("---- %d %ld [%lld %lld] (%lld %lld) |%ld %ld|\n",res,pos,l.b,r.b,*l.v,*r.v,min_size,max_size); fflush(stdout);
         
-        //printf("---- %d %ld [%lld %lld] (%lld %lld) |%ld %ld|\n",res,pos,l.b,r.b,*l.v,*r.v,min_size,max_size); fflush(stdout);
-//        if(nearest_pos){
-//        }
-        offset_type nearest_pos=0;value_type nearest_value=0;       
-        offset_type next_pos=0;value_type next_value=0;       
-        int direction=0;{
-            auto a = (want>*r.v)*(want - *r.v)+!(want>*r.v)*(*r.v-want); 
-            auto b = (want>*l.v)*(want - *l.v)+!(want>*l.v)*(*l.v-want); 
-            nearest_pos = static_cast<offset_type>(!(a<b))*l.b + static_cast<offset_type>(a<b)*r.b; // if abs is equal, l is prior 
-            nearest_value = !(a<b)* *l.v + (a<b)* *r.v; // if abs is equal, l is prior 
-            direction = !exact*res;
-            
-            next_pos = static_cast<offset_type>(!(a<b))*(nearest_pos+block_size) + static_cast<offset_type>(a<b)*(nearest_pos-block_size); 
-            
-            value_type * next_value_ptr=0;
-            prf->read_value(next_pos,&next_value_ptr);
-            next_value=*next_value_ptr;
-            
-        }
-        
-        
-        //auto next_pos = nearest_pos+(direction*block_size);
-        //auto next_value = value_type{};
-        
-
-        auto result = btree_search_result{};
         {
-            {
-                auto ptr = (offset_type *) (uintptr_t(nearest_value >= want)*uintptr_t(&result.lpos) + !uintptr_t(nearest_value >= want)*uintptr_t(&result.rpos));
-                *ptr = nearest_pos;
+            auto result = btree_search_result{};
+            result.direction = !exact*res;
+            if(detail){
+                auto a = (want>*r.v)*(want - *r.v)+!(want>*r.v)*(*r.v-want); 
+                auto b = (want>*l.v)*(want - *l.v)+!(want>*l.v)*(*l.v-want); 
+                result.pos = static_cast<offset_type>(!(a<b))*l.b + static_cast<offset_type>(a<b)*r.b; // if abs is equal, l is prior 
+                result.pos = (result.pos>=upper_limit_size)*(upper_limit_size-block_size) + !(result.pos>=upper_limit_size)*result.pos; 
+                result.value = !(a<b)* *l.v + (a<b)* *r.v; // if abs is equal, l is prior 
+                result.overflow = is_overflow_max+is_overflow_min;
+                if(!exact*!result.overflow){
+                    result.next_pos = static_cast<offset_type>(!(a<b))*(result.pos+block_size) + static_cast<offset_type>(a<b)*(result.pos-block_size); 
+                    value_type * next_value_ptr=0;
+                    prf->read_value(result.next_pos,&next_value_ptr);
+                    result.next_value=*next_value_ptr;
+                }
             }
-            
-            {
-                auto ptr = (offset_type *) (uintptr_t(nearest_value >= want)*uintptr_t(&result.lv) + !uintptr_t(nearest_value >= want)*uintptr_t(&result.rv));
-                *ptr = nearest_value;
-            }
-        }
-
-        {
-            {
-                auto ptr = (offset_type *) (uintptr_t(next_value >= want)*uintptr_t(&result.lpos) + !uintptr_t(next_value >= want)*uintptr_t(&result.rpos));
-                *ptr = next_pos;
-            }
-            
-            
-            {
-                auto ptr = (offset_type *) (uintptr_t(next_value >= want)*uintptr_t(&result.lv) + !uintptr_t(next_value >= want)*uintptr_t(&result.rv));
-                *ptr = next_value;
-            }
+            return result;
         }
         
-//        result.lv = *l.v;
-//        result.lpos = l.b;
-//        result.rv = *r.v;
-//        result.lpos = r.b;
-        result.found = exact;
-        return result;
-        
-        
-        
-        
-        //return exact;
     }
     
 private:
--- btree_search/unit_test.cc
+++ btree_search/unit_test.cc
@@ -64,43 +64,27 @@ int file_syscall_16b(uint64_t const & search_v){
         auto bt = kautil::algorithm::btree_search{&pref};
         auto pos = file_syscall_16b_pref::offset_type(0);
         auto dir = int(0);
-        auto found = bt.search(static_cast<file_syscall_16b_pref::value_type>(search_v),&pos,&dir);
-        printf("%s. pos is %ld. direction is %d\n",found?"found": "not found",pos,dir);
-
-        return 0;
-    }
-}
-
-int memory_8b(uint64_t const& search_v ){
-    struct memory_8b_arr_pref{
-        using value_type = uint64_t;
-        using offset_type = uint64_t;
-    
-        value_type * data =0;
-        offset_type size = 0;
         
-        offset_type block_size(){ return sizeof(value_type); }
-        offset_type max_size(){ return size; }
-        void read_value(offset_type const& offset, value_type ** value){ 
-            *value=&data[offset/sizeof(value_type)]; 
+        
+        {
+            auto res = bt.search(static_cast<file_syscall_16b_pref::value_type>(search_v));
+            if(!res.direction){
+                printf("found. pos is %lld. value is %lld.\n",res.pos,res.value);
+            }else{
+                printf("not found. nearest   pos is %lld. nearest   value is %lld.\n"
+                       ,res.pos,res.value);
+                printf((res.direction<0)?
+                        "not found. next      pos is %lld. next      value is %lld.\n"
+                       :"not found. before    pos is %lld. before    value is %lld.\n"
+                       ,res.next_pos,res.next_value);
+            }
+            return !res.direction;
         }
-    };
-
-    constexpr auto len = 100; 
-    memory_8b_arr_pref::value_type data[len];
-    //auto search_v=memory_8b_arr_pref::value_type(550);
-    auto cnt = 0;
-    for(auto i = 0; i <len; ++i){
-        data[i] = i * 10;
-    }
-    auto pref = memory_8b_arr_pref{.data=data,.size=sizeof(data)};
-    {// main
-        auto pos = memory_8b_arr_pref::offset_type(0);
-        auto dir = int(0);
-        auto bt = kautil::algorithm::btree_search{&pref};
-        auto found = bt.search(static_cast<memory_8b_arr_pref::value_type>(search_v),&pos,&dir);
-        printf("%s. pos is %lld. direction is %d\n",found?"found": "not found",pos,dir);fflush(stdout);
-        return found;
+        
+//        auto res = bt.search(static_cast<file_syscall_16b_pref::value_type>(search_v));
+//        printf("%s. pos is %lld. direction is %d\n",!res.direction?"found": "not found",res.pos,res.direction);
+//        return !res.direction;
+        return 0;
     }
 }
 
@@ -121,33 +105,33 @@ int memory_16b(uint64_t const& search_v){
     };
     
     
+//    not found. nearest   pos is 792. nearest   value is 990.
+//    not found. before    pos is 0    before    value is 0.
+
+    
     constexpr auto len = 100; 
     memory_16b_arr_pref::value_type data[len*2];
     auto cnt = 0;
     for(auto i = 0; i <len*2; i+=2){
         data[i] = i*10;
         data[i+1] = data[i]+10;
-        //printf("%lld %lld\n",data[i],data[i+1]);
     }
     
     { // main 
         auto pref = memory_16b_arr_pref{.data=data,.size=sizeof(data)};
-        auto pos = memory_16b_arr_pref::offset_type(0);
-        auto dir = int(0);
         auto bt = kautil::algorithm::btree_search{&pref};
-
         {
-            auto found = bt.search(static_cast<memory_16b_arr_pref::value_type>(search_v),&pos,&dir);
-            printf("%s. pos is %lld. direction is %d\n",found?"found": "not found",pos,dir);
-            //return found;
-        }
-        
-        {
-            auto res = bt.__search(static_cast<memory_16b_arr_pref::value_type>(search_v));
-            int jjj = 0;
-            
-//            printf("%s. pos is %lld. direction is %d\n",found?"found": "not found",pos,dir);
-//            return found;
+            auto res = bt.search(static_cast<memory_16b_arr_pref::value_type>(search_v));
+            if(!res.direction){
+                printf("found. pos is %lld. value is %lld.\n",res.pos,res.value);
+            }else{
+                printf("not found. nearest   pos is %lld. nearest   value is %lld.\n"
+                       ,res.pos,res.value);
+                printf((res.direction<0)?
+                        "not found. next      pos is %lld. next      value is %lld.\n"
+                       :"not found. before    pos is %lld. before    value is %lld.\n"
+                       ,res.next_pos,res.next_value);
+            }
         }
         
         
@@ -157,18 +141,77 @@ int memory_16b(uint64_t const& search_v){
 }
 
 
+int memory_8b(uint64_t const& search_v ){
+    struct memory_8b_arr_pref{
+        using value_type = uint64_t;
+        using offset_type = uint64_t;
+    
+        value_type * data =0;
+        offset_type size = 0;
+        
+        offset_type block_size(){ return sizeof(value_type); }
+        offset_type max_size(){ return size; }
+        void read_value(offset_type const& offset, value_type ** value){ 
+            *value=&data[offset/sizeof(value_type)]; 
+        }
+    };
+
+//    constexpr auto len = 100; 
+//    memory_8b_arr_pref::value_type data[len*2];
+//    //auto search_v=memory_8b_arr_pref::value_type(550);
+//    auto cnt = 0;
+//    for(auto i = 0; i <len; i+=2){
+//        data[i] = i * 10;
+//        data[i+1] = data[i]+10;
+//    }
+    
+    constexpr auto len = 100; 
+    memory_8b_arr_pref::value_type data[len];
+    auto cnt = 0;
+    for(auto i = 0; i <len; ++i){
+        data[i] = i*10;
+    }
+    
+    auto pref = memory_8b_arr_pref{.data=data,.size=sizeof(data)};
+    {// main
+        auto pos = memory_8b_arr_pref::offset_type(0);
+        auto dir = int(0);
+        auto bt = kautil::algorithm::btree_search{&pref};
+        
+//        auto res = bt.search(static_cast<memory_8b_arr_pref::value_type>(search_v));
+//        printf("%s. pos is %lld. direction is %d\n",!res.direction?"found": "not found",res.pos,res.direction);
+        {
+            auto res = bt.search(static_cast<memory_8b_arr_pref::value_type>(search_v));
+            if(!res.direction){
+                printf("found. pos is %lld. value is %lld.\n",res.pos,res.value);
+            }else{
+                printf("not found. nearest   pos is %lld. nearest   value is %lld.\n"
+                       ,res.pos,res.value);
+                printf((res.direction<0)?
+                        "not found. next      pos is %lld. next      value is %lld.\n"
+                       :"not found. before    pos is %lld. before    value is %lld.\n"
+                       ,res.next_pos,res.next_value);
+                if(res.overflow){
+                    printf("overflow %s",res.overflow ? ((res.direction>0)?"upper":"lower"):"nothing" );
+                }
+            }
+            return !res.direction;
+        }
+    }
+}
+
 
 int main(){
     
     
-    auto search_v = uint64_t(980); // todo: valid but not sufficient. i also want both l and r values of last calc.  
-    memory_16b(search_v);
-    return (0);
+    auto search_v = uint64_t(999); // todo: valid but not sufficient. i also want both l and r values of last calc.  
+//    memory_16b(search_v);
+//    return (0);
     
     {
         //  16 bytes array
-        file_syscall_16b(search_v);
-        memory_16b(search_v);
+//        file_syscall_16b(search_v);
+//        memory_16b(search_v);
         
         // 8 bytes array
         memory_8b(search_v);
